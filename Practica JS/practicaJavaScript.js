026fa04ff936da39d7117d6036aed05b60de1a37
AnaPaolaJuarezSanchez <94088458+AnaPaolaJuarezSanchez@users.noreply.github.com>
2021-11-10 23:26:18 -0700

    Add files via upload

---
+++ Practica JS/practicaJavaScript.js
@@ -0,0 +1,9 @@
+console.log("Se cargo el archivo de Javascript");
+
+var contador = 0;
+      
+function sumaContador(){
+	contador++;
+	console.log(contador);
+	document.getElementById("contador").innerHTML = contador;
+}
